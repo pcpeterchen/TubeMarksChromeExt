cf269f5d2a6c963ad86503074378d2bf3959b032
Peter Chen <pcchen@princeton.edu>
2016-08-03 17:15:23 -0400

    Added keypress, data logging, and clearing.

--- myscript.js
+++ myscript.js
@@ -1,4 +1,21 @@
-//Gets the Youtube Video  Current Time
-stuff = document.getElementsByClassName("ytp-time-current")[0].innerHTML;
-console.log(stuff);
-alert(stuff);
+document.body.addEventListener('keyup', function(e) {
+	if (e.keyCode == 67) {
+		var timevalue = document.getElementsByClassName("ytp-time-current")[0].innerHTML;	
+		var time = timevalue;
+		var obj = {};
+		obj[time] = timevalue;
+		chrome.storage.local.set(obj);
+		console.log(time);
+	}
+});
+
+document.body.addEventListener('keyup', function(e) {
+	if (e.keyCode == 82) {
+		chrome.storage.local.get(null, function(items) {
+			var allKeys = Object.keys(items);
+			for(var key in allKeys) {
+				console.log(allKeys[key]);
+			}
+		});
+	}
+});
--- popup.js
+++ popup.js
@@ -2,13 +2,17 @@ function renderStatus(statusText) {
   document.getElementById('status').textContent = statusText;
 }
 
-  document.addEventListener('DOMContentLoaded', function() {
-    renderStatus('rekt');
-    var button = document.getElementById('button');
-    button.addEventListener('click', function() {
-      chrome.tabs.executeScript({
-        file: 'myscript.js'
-      });
-      renderStatus('memes');
+document.addEventListener('DOMContentLoaded', function() {
+  renderStatus('rekt');
+  var button = document.getElementById('button');
+  button.addEventListener('click', function() {
+    chrome.storage.local.clear(function(obj){
+      console.log("cleared");
     });
+    renderStatus('memes');
   });
+
+  chrome.tabs.executeScript({
+    file: 'myscript.js'
+  });
+});
