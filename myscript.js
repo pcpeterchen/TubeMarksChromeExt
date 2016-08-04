de890027cb3bcc49c0c1b1e485defeef0ae1a293
Peter Chen <pcchen@princeton.edu>
2016-08-04 18:09:46 -0400

    Added local storage to specific websites.

--- myscript.js
+++ myscript.js
@@ -1,21 +1,36 @@
+
 document.body.addEventListener('keyup', function(e) {
-	if (e.keyCode == 67) {
-		var timevalue = document.getElementsByClassName("ytp-time-current")[0].innerHTML;	
-		var time = timevalue;
-		var obj = {};
-		obj[time] = timevalue;
-		chrome.storage.local.set(obj);
-		console.log(time);
+	if (e.keyCode == 83) {
+		var	site = document.URL;
+		time = document.getElementsByClassName("ytp-time-current")[0].innerHTML;
+		x = localStorage.getItem(site);
+		if (x == null) {
+			var timearray = [];
+			timearray.push(time);
+			localStorage.setItem(site, JSON.stringify(timearray));
+			console.log(timearray);
+		} 
+		else {
+			timearray = JSON.parse(localStorage.getItem(site));
+			console.log(timearray);
+			timearray.push(time);
+			localStorage.setItem(site, JSON.stringify(timearray));
+			console.log(timearray);
+		}
 	}
 });
 
 document.body.addEventListener('keyup', function(e) {
-	if (e.keyCode == 82) {
-		chrome.storage.local.get(null, function(items) {
-			var allKeys = Object.keys(items);
-			for(var key in allKeys) {
-				console.log(allKeys[key]);
-			}
-		});
+	if (e.keyCode == 71) {
+		var	site = document.URL;
+		console.log(site);
+		console.log(localStorage.getItem(site));
+	}
+});
+
+document.body.addEventListener('keyup', function(e) {
+	if (e.keyCode == 69) {
+		var	site = document.URL;
+		localStorage.removeItem(site);
 	}
 });
