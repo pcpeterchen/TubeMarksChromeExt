8752a89afc2720ef1204062eea1d02c3028969c4
Peter Chen <pcchen@princeton.edu>
2016-08-05 17:09:26 -0400

    Added ability to display info on youtbe pages.

--- myscript.js
+++ myscript.js
@@ -1,3 +1,26 @@
+// request permission on page load
+document.addEventListener('DOMContentLoaded', function () {
+  if (Notification.permission !== "granted")
+    Notification.requestPermission();
+});
+
+function notifyMe() {
+  if (!Notification) {
+    alert('Desktop notifications not available in your browser. Try Chromium.'); 
+    return;
+  }
+
+  if (Notification.permission !== "granted")
+    Notification.requestPermission();
+  else {
+    var notification = new Notification('Notification title', {
+      icon: 'https://yt3.ggpht.com/-p-O21kZZDPs/AAAAAAAAAAI/AAAAAAAAAAA/an-88_jZWFI/s48-c-k-no-rj-c0xffffff/photo.jpg',
+      body: "TubeMarks Erased!",
+    });    
+  }
+
+}
+
 
 document.body.addEventListener('keyup', function(e) {
 	if (e.keyCode == 83) {
@@ -12,9 +35,10 @@ document.body.addEventListener('keyup', function(e) {
 		} 
 		else {
 			timearray = JSON.parse(localStorage.getItem(site));
-			console.log(timearray);
-			timearray.push(time);
-			localStorage.setItem(site, JSON.stringify(timearray));
+			if (timearray.indexOf(time) == -1){
+				timearray.push(time);
+				localStorage.setItem(site, JSON.stringify(timearray));
+			}
 			console.log(timearray);
 		}
 	}
@@ -24,7 +48,26 @@ document.body.addEventListener('keyup', function(e) {
 	if (e.keyCode == 71) {
 		var	site = document.URL;
 		console.log(site);
-		console.log(localStorage.getItem(site));
+		timearray = JSON.parse(localStorage.getItem(site));
+		console.log(timearray);
+
+		var myElem = document.getElementById(site);
+		if (myElem == null) {
+		     //Create Labels
+		     var myDiv = document.createElement("div");
+		     myDiv.setAttribute("id", site);
+		     myDiv.setAttribute("style", "font-weight:normal");
+		     myDiv.setAttribute("cssFloat", "left");
+		     myDiv.innerHTML = timearray;
+
+	          // 'foobar' is the div id, where new fields are to be added
+	          var foo = document.getElementById("watch7-views-info");
+
+	          //Append the element in page (in span).
+	          foo.appendChild(myDiv);
+	   }
+	   myElem.innerHTML = timearray;
+
 	}
 });
 
@@ -32,5 +75,8 @@ document.body.addEventListener('keyup', function(e) {
 	if (e.keyCode == 69) {
 		var	site = document.URL;
 		localStorage.removeItem(site);
+		var myElem = document.getElementById(site);
+		myElem.innerHTML = '';
 	}
 });
+
